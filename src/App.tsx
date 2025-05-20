60fcacb4ef60dfae7c761e4b019a4a6f0bf260b3
cr1sostomo <lucascrisostomo9120@gmail.com>
2025-05-20 20:57:25 -0300

    Mudanças da noite

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text lg font-bold text-red-700">cheguei</h1>
-        <div className="bg-gray-400 shadow-md rounded-lg p-4 m-4">
-          <h2 className="text-2xl" >
-            É um fato conhecido de todos que um leitor se distrairá com o
-            conteúdo de texto legível de uma página quando estiver examinando
-            sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma
-            distribuição normal de letras, ao contrário de "Conteúdo aqui,
-            conteúdo aqui", fazendo com que ele tenha uma aparência similar a de
-            um texto legível. Muitos softwares de publicação e editores de
-            páginas na internet agora usam Lorem Ipsum como texto-modelo padrão,
-            e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em
-            sua fase de construção. Várias versões novas surgiram ao longo dos
-            anos, eventualmente por acidente, e às vezes de propósito (injetando
-            humor, e coisas do gênero).
-          </h2>
-        </div>
+      <div className=" flex flex-col m-4 p-4 text-center items-center">
+        <div className=" bg-blue-400 w-1/2"  > 01</div>
+        <div className=" bg-red-400">02</div>
+        <div className=" bg-yellow-400 w-full">03</div> 
+
       </div>
     </>
   );
